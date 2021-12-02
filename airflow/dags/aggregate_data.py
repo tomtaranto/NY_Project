26f555fea297590cc6de89a126c169cbfc64a9d0
ttaranto <tom.taranto@yahoo.fr>
2021-12-02 22:38:16 +0100

    compatibilité avec S3 etdynamodb fonctionnel

--- airflow/dags/aggregate_data.py
+++ airflow/dags/aggregate_data.py
@@ -36,20 +36,15 @@ def dag_projet():
             "s3",
             aws_access_key_id=AWS_ACCESS_KEY,
             aws_secret_access_key=AWS_SECRET_KEY,
-            region_name="eu-west-1"
+            region_name="eu-west-3"
         )
         month = date[-5:-3]
-        year = date[0:5]
+        year = date[0:4]
         s3_filename = 'yellow_tripdata_' + year + '-' + month + '.csv'
         local_filename = '/tmp/' + s3_filename
-        s3.download_file('projet-airflow-airbnb-esgi-2021', s3_filename,
-                         local_filename)  # TODO change bucket name and file name, utiliser un try,except pour dl le fichier?
-        try:
-            s3.Bucket(AWS_S3_BUCKET_NAME).download_file('projet-airflow-airbnb-esgi-2021', s3_filename,
-                                                        local_filename)
-        except botocore.exceptions.ClientError as e:
-            print("Returned error S3 : " + e.response['Error']['Code'])
-
+        print("s3_filename",s3_filename)
+        s3.download_file('esginyprojectrawdata', s3_filename,
+                         local_filename)
         return dict(local_filename=local_filename)
 
     @task()
@@ -67,7 +62,7 @@ def dag_projet():
 
         # On recupere uniquement le jour qui nous interesse
         day = int(date[-2:])
-        print("day : ",day)
+        print("day : ", day)
         print(monthly_data)
         daily_data = monthly_data[monthly_data["day"] == day]
         daily_data["month"] = int(date[-5:-3])
@@ -76,8 +71,8 @@ def dag_projet():
         res = daily_data[["VendorID", "day", "month", "total_amount"]].groupby(
             by=["VendorID", "day"]).mean().reset_index()
 
-        res.to_csv("/tmp/yellow_cab.csv", index=False)
-        return "/tmp/yellow_cab.csv"
+        res.to_csv("/tmp/yellow_cab_1_"+str(day)+".csv", index=False)
+        return "/tmp/yellow_cab_1_"+str(day)+".csv"
 
     @task()
     def load(filepath):  # TODO faire l'insertion dans la table dynamoDB
@@ -91,17 +86,19 @@ def dag_projet():
             "dynamodb",
             aws_access_key_id=AWS_ACCESS_KEY,
             aws_secret_access_key=AWS_SECRET_KEY,
-            region_name="eu-west-1"
+            region_name="eu-west-3"
         )
-        table = dynamodb.Table("daily_ca")
+        table = dynamodb.Table("table2")
 
         def put_row(row, batch):
+            id = str(int(row.name)+int(row["month"])*100+int(float(row["day"]))+1000*int(row["VendorID"]))
             batch.put_item(
                 Item={
                     'vendorid': int(row["VendorID"]),
                     'day': int(row["day"]),
                     'month': int(row["month"]),
-                    'total_amount': Decimal(str(row["total_amount"]))
+                    'total_amount': Decimal(str(row["total_amount"])),
+                    'id' : id
                 }
             )
 
@@ -125,90 +122,93 @@ def dag_projet_2():
 
     # Charge les données depuis S3
     @task()
-    def extract_2():  # TODO passer la date en parametre et recuperer uniquement le fichier du mois
+    def extract_2(
+            date):
         s3 = boto3.client(
             "s3",
             aws_access_key_id=AWS_ACCESS_KEY,
             aws_secret_access_key=AWS_SECRET_KEY,
-            region_name="eu-west-1"
+            region_name="eu-west-3"
         )
-        s3.download_file('projet-airflow-airbnb-esgi-2021', 'yellow_tripdata_2019-01.csv',
-                         '/tmp/yellow_tripdata_2019-01.csv')  # TODO change bucket name
-        s3.download_file('projet-airflow-airbnb-esgi-2021', 'yellow_tripdata_2019-02.csv',
-                         '/tmp/yellow_tripdata_2019-02.csv')  # TODO change bucket name
-        return dict(january="/tmp/yellow_tripdata_2019-01.csv", february="/tmp/yellow_tripdata_2019-02.csv")
+        month = date[-5:-3]
+        year = date[0:4]
+        s3_filename = 'yellow_tripdata_' + year + '-' + month + '.csv'
+        local_filename = '/tmp/' + s3_filename
+        print("s3_filename : ",s3_filename)
+        s3.download_file('esginyprojectrawdata', s3_filename,
+                         local_filename)
+        return dict(local_filename=local_filename)
 
     @task()
-    def transform_2(paths=None):  # TODO supprimer le deuxieme mois, enlever le concat, rename les variables
-        if paths is None:
-            paths = dict(
-                january="/home/noobzik/Documents/ESGI/5A/S1/5-AWS/projet/NY_Project/yellow_tripdata_2019-02.csv",
-                february="/home/noobzik/Documents/ESGI/5A/S1/5-AWS/projet/NY_Project/yellow_tripdata_2019-02.csv")
+    def transform_2(date, paths=None):  # TODO supprimer le deuxieme mois, enlever le concat, rename les variables
+        if paths is None:  # Pour tester
+            paths = dict(local_filename='~/PycharmProjects/NY_Project/data/yellow_tripdata_2019-01.csv')
+
         print("paths", paths)
-        january = pd.read_csv(paths["january"], sep=",", header=0)
-        february = pd.read_csv(paths["february"], sep=",", header=0)
-        print(february)
-        february["tpep_pickup_datetime"] = pd.to_datetime(february["tpep_pickup_datetime"])
-        february["day"] = february["tpep_pickup_datetime"].dt.day
-        print(february.PULocationID.max())
-        print("----------------------")
-        print(february.DOLocationID.max())
+        monthly_data = pd.read_csv(paths["local_filename"], sep=",", header=0)
 
-        january["tpep_pickup_datetime"] = pd.to_datetime(january["tpep_pickup_datetime"])
-        january["day"] = january["tpep_pickup_datetime"].dt.day
+        monthly_data["tpep_pickup_datetime"] = pd.to_datetime(monthly_data["tpep_pickup_datetime"])
+        monthly_data["day"] = monthly_data["tpep_pickup_datetime"].dt.day
+        print("date : ", date, date[-2:])
+        daily_data = monthly_data[monthly_data["day"] == int(date[-2:])]
 
-        grouped_id_day_january_1 = january[["DOLocationID", "day"]].groupby(
+        # Recherche des zones de départ
+        grouped_id_day_monthly_1 = daily_data[["DOLocationID", "day"]].groupby(
             by=["DOLocationID", "day"]).size().reset_index(name='counts1')
 
-        grouped_id_day_january_2 = january[["PULocationID", "day"]].groupby(
+        # REcherche des zones d'arrivée
+        grouped_id_day_monthly_2 = daily_data[["PULocationID", "day"]].groupby(
             by=["PULocationID", "day"]).size().reset_index(name='counts2')
 
-        grouped_id_day_january_3 = january.loc[january['DOLocationID'] == january['PULocationID']]
-        grouped_id_day_january_3 = grouped_id_day_january_3[["PULocationID", "day"]].groupby(
+        # Recherche des zones avec depart == arrive
+        grouped_id_day_monthly_3 = daily_data.loc[daily_data['DOLocationID'] == daily_data['PULocationID']]
+        grouped_id_day_monthly_3 = grouped_id_day_monthly_3[["PULocationID", "day"]].groupby(
             by=["PULocationID", "day"]).size().reset_index(name='counts3')
 
-        grouped_id_day_january_1 = grouped_id_day_january_1.rename({'DOLocationID': 'PULocationID'}, axis=1)
+        grouped_id_day_monthly_1 = grouped_id_day_monthly_1.rename({'DOLocationID': 'PULocationID'}, axis=1)
 
-        new_df = grouped_id_day_january_1.merge(grouped_id_day_january_2, how='inner', on=['PULocationID', 'day'])
-        new_df = new_df.merge(grouped_id_day_january_3, how='inner', on=['PULocationID', 'day'])
+        new_df = grouped_id_day_monthly_1.merge(grouped_id_day_monthly_2, how='inner', on=['PULocationID', 'day'])
+        new_df = new_df.merge(grouped_id_day_monthly_3, how='inner', on=['PULocationID', 'day'])
 
         new_df['true_count'] = new_df['counts1'] + new_df['counts2'] - new_df['counts3']
 
-        print(grouped_id_day_january_1)
-        print(grouped_id_day_january_2)
-        print(grouped_id_day_january_3)
-        print(new_df)
         new_df = new_df[["day", "PULocationID", "true_count"]].rename(
             {'PULocationID': 'LocationID', 'true_count': 'count'}, axis=1)
-        # res = pd.concat([grouped_id_day_january, grouped_id_day_february], ignore_index=True)
-        new_df.to_csv("/tmp/yellow_cab_2.csv", index=False)
-        return "/tmp/yellow_cab_2.csv"
+        new_df["month"] = int(date[-5:-3])
+        new_df.to_csv("/tmp/yellow_cab_2_"+date[-2:]+".csv", index=False)
+        return "/tmp/yellow_cab_2_"+date[-2:]+".csv"
 
     @task()
     def load_2(filepath):  # TODO faire l'insertion dans la table dynamoDB
-        df = pd.read_csv(filepath).head(n=100)
+        if filepath is None:
+            filepath = '/tmp/yellow_cab_2.csv'
+        df = pd.read_csv(filepath, sep=',', header=0).head(n=100)
         dynamodb = boto3.resource(
             "dynamodb",
             aws_access_key_id=AWS_ACCESS_KEY,
             aws_secret_access_key=AWS_SECRET_KEY,
-            region_name="eu-west-1"
+            region_name="eu-west-3"
         )
-        table = dynamodb.Table("listings_prices")
-
+        table = dynamodb.Table("esgi_data_2021")
         def put_row(row, batch):
-            batch.put_item(
+            index = row.index
+            month = row["month"]
+            day = row["day"]
+            res = batch.put_item(
                 Item={
-                    'listing_id': int(row["listing_id"]),
-                    'week': int(row["week"]),
-                    'price': Decimal(str(row["price"]))
+                    'month': int(row["month"]),
+                    'day': int(row["day"]),
+                    'location': int(row["LocationID"]),
+                    'count': Decimal(str(row["count"])),
+                    'id' : str(int(row.name)+int(row["month"])*100+int(float(row["day"])))
                 }
             )
 
         with table.batch_writer() as batch:
             df.apply(lambda x: put_row(x, batch), axis=1)
 
-    paths = extract_2()
-    filepath = transform_2(paths)
+    paths = extract_2("{{ yesterday_ds }}")
+    filepath = transform_2("{{ yesterday_ds }}",paths)
     load_2(filepath)
 
 
